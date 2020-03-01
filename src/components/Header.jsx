12a758559df532632204976b57e0dbc7a1b1a7fe
fishnal <vpatel14@terpmail.umd.edu>
2020-02-29 19:17:17 -0500

    Renamed var

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default class Header extends React.Component {
   render () {
-    const s = '<video autoplay muted loop class="header-video"><source src="videos/bg-video.mp4"></source></video>'
+    const videoHtml = '<video autoplay muted loop class="header-video"><source src="videos/bg-video.mp4"></source></video>'
 
     return (
       <div className='header-bg'>
-        <div dangerouslySetInnerHTML={{ __html: s }}></div>
+        <div dangerouslySetInnerHTML={{ __html: videoHtml }}></div>
         <div className='header-content'>
           <div id="title-text">Koreography</div>
           <div id="subtitle-text">Student K-Pop Groups at UMD</div>
