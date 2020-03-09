182ec7e2c7006b90ed82cdf10aafd2e32ff2d630
fishnal <vpatel14@terpmail.umd.edu>
2020-03-09 19:52:49 -0400

    Added soundcloud embed class

--- src/components/Podcast.jsx
+++ src/components/Podcast.jsx
@@ -5,8 +5,7 @@ export default class Podcast extends React.Component {
   render() {
     return (
       <iframe
-        width='100%'
-        height='166px'
+        className='soundcloud-embed'
         src={this.props.src}
         title={Buffer.from(this.props.src).toString('base64')}
         scrolling='no'
