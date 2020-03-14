a47e5d389d5f7731ecd98ce05e7d415ec5e60fcf
fishnal <vpatel14@terpmail.umd.edu>
2020-03-13 21:22:30 -0400

    Video force plays for safari

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default class Header extends React.Component {
   render() {
-    const videoHtml = '<video autoplay playsinline muted loop class="header-video"><source src="videos/bg-video.mp4"></source></video>'
+    const videoHtml = '<video id="bg-video" autoplay playsinline muted loop class="header-video"><source src="videos/bg-video.mp4"></source></video>'
 
     return (
       <div className='header-bg'>
@@ -13,5 +13,22 @@ export default class Header extends React.Component {
         </div>
       </div>
     )
+  }
+
+  componentDidMount() {
+    const ua = navigator.userAgent.toLowerCase()
+    const isSafari = ua.indexOf('safari/') >= 0 && ua.indexOf('chrome') < 0
+
+    // Force autoplay on safari
+    if (isSafari) {
+      const video = document.getElementById('bg-video')
+
+      // Only force play video if muted and has autoplay
+      if (video instanceof HTMLMediaElement && video.muted && video.autoplay) {
+        setTimeout(() => {
+          video.play()
+        }, 50)
+      }
+    }
   }
 }
